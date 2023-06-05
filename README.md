6d760405ebce07b83aa1dd5bf318c1d780ed9ceb
mariana-cunha <mari.marques26@gmail.com>
2023-06-04 21:20:21 -0300

    first commit - entrega

---
+++ README.md
@@ -0,0 +1,17 @@
+# RD Challenge - Javascript
+
+## Para rodar o projeto
+```
+
+cd javascript
+yarn install
+
+```
+
+### Para rodar os testes na pasta
+
+```
+
+yarn test
+
+```
---
+++ javascript/customer-success-balancing.js
@@ -0,0 +1,146 @@
+/**
+ * Returns the id of the CustomerSuccess with the most customers
+ * @param {array} customerSuccess
+ * @param {array} customers
+ * @param {array} customerSuccessAway
+ */
+function customerSuccessBalancing(
+  customerSuccess,
+  customers,
+  customerSuccessAway
+) {
+  // Filter which CSs are available
+  const activeCSS = customerSuccess
+  .filter(cs => !customerSuccessAway.includes(cs.id));
+
+  // Set an order
+  const cssAvailable = customerSuccess
+  .filter((e) => !customerSuccessAway.includes(e.id))
+  .sort((a, b) => a.score - b.score);
+
+  // Array counting starts at 1 since we'll check for ties later on
+  const customerCounting = new Array(activeCSS.length).fill(1);
+
+  // Find the CS with the maximum score
+  customers.forEach((customer) => {
+    for (let i = 0; i < cssAvailable.length; i++) {
+      if (customerCounting[i] === 0 || customer.score <= cssAvailable[i].score) {
+        customerCounting[i]++;
+        break;
+      }
+    }
+  });
+
+  // 0 < id do cliente < 1.000.000
+  const maxCSs = Math.max(...customerCounting);
+  const redirectsCostumersToCS = cssAvailable
+  .filter((cs, index) => customerCounting[index] === maxCSs);
+  
+  // Check for ties
+  if (redirectsCostumersToCS.length > 1) {
+    return 0
+  } else {
+    return redirectsCostumersToCS[0].id;
+  }
+}
+
+test("Scenario 1", () => {
+  const css = [
+    { id: 1, score: 60 },
+    { id: 2, score: 20 },
+    { id: 3, score: 95 },
+    { id: 4, score: 75 },
+  ];
+  const customers = [
+    { id: 1, score: 90 },
+    { id: 2, score: 20 },
+    { id: 3, score: 70 },
+    { id: 4, score: 40 },
+    { id: 5, score: 60 },
+    { id: 6, score: 10 },
+  ];
+  const csAway = [2, 4];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(1);
+});
+
+function buildSizeEntities(size, score) {
+  const result = [];
+  for (let i = 0; i < size; i += 1) {
+    result.push({ id: i + 1, score });
+  }
+  return result;
+}
+
+function mapEntities(arr) {
+  return arr.map((item, index) => ({
+    id: index + 1,
+    score: item,
+  }));
+}
+
+function arraySeq(count, startAt){
+  return Array.apply(0, Array(count)).map((it, index) => index + startAt);
+}
+
+test("Scenario 2", () => {
+  const css = mapEntities([11, 21, 31, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
+});
+
+test("Scenario 3", () => {
+  const testTimeoutInMs = 100;
+  const testStartTime = new Date().getTime();
+
+  const css = mapEntities(arraySeq(999, 1));
+  const customers = buildSizeEntities(10000, 998);
+  const csAway = [999];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(998);
+
+  if (new Date().getTime() - testStartTime > testTimeoutInMs) {
+    throw new Error(`Test took longer than ${testTimeoutInMs}ms!`);
+  }
+});
+
+test("Scenario 4", () => {
+  const css = mapEntities([1, 2, 3, 4, 5, 6]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
+});
+
+test("Scenario 5", () => {
+  const css = mapEntities([100, 2, 3, 6, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(1);
+});
+
+test("Scenario 6", () => {
+  const css = mapEntities([100, 99, 88, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [1, 3, 2];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
+});
+
+test("Scenario 7", () => {
+  const css = mapEntities([100, 99, 88, 3, 4, 5]);
+  const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
+  const csAway = [4, 5, 6];
+
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(3);
+});
+
+test("Scenario 8", () => {
+  const css = mapEntities([60, 40, 95, 75]);
+  const customers = mapEntities([90, 70, 20, 40, 60, 10]);
+  const csAway = [2, 4];
+  expect(customerSuccessBalancing(css, customers, csAway)).toEqual(1);
+});
